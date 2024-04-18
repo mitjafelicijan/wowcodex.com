0c4c3bbf6ad1233cc8b07a09b6be3049349b7de0
Mitja Felicijan <mitja.felicijan@gmail.com>
2024-04-18 07:57:42 +0200

    Updated helpers

--- apiref/helpers.lua
+++ apiref/helpers.lua
@@ -3,59 +3,59 @@ local lfs = require("lfs")
 local Helpers = {}
 
 Helpers.printTable = function(tbl, indent)
-	indent = indent or 0
-	for k, v in pairs(tbl) do
-		local formatting = string.rep("  ", indent) .. tostring(k) .. ": "
-		if type(v) == "table" then
-			print(formatting)
-			printTable(v, indent + 1)
-		else
-			print(formatting .. tostring(v))
-		end
-	end
+  indent = indent or 0
+  for k, v in pairs(tbl) do
+    local formatting = string.rep("  ", indent) .. tostring(k) .. ": "
+    if type(v) == "table" then
+      print(formatting)
+      printTable(v, indent + 1)
+    else
+      print(formatting .. tostring(v))
+    end
+  end
 end
 
 Helpers.fileExists = function(filePath)
-	local file = io.open(filePath, "r")
-	if file then
-		file:close()
-		return true
-	else
-		return false
-	end
+  local file = io.open(filePath, "r")
+  if file then
+    file:close()
+  return true
+  else
+    return false
+  end
 end
 
 Helpers.readFile = function(filePath)
-	local file = io.open(filePath, "r")
-	if not file then
-		return nil, "Unable to open file"
-	end
+  local file = io.open(filePath, "r")
+  if not file then
+    return nil, "Unable to open file"
+  end
 
-	local content = file:read("*a")
-	file:close()
+  local content = file:read("*a")
+  file:close()
 
-	return content
+  return content
 end
 
 Helpers.writeFile = function(filePath, content)
-	local file = io.open(filePath, "w")
-	if not file then
-		return false, "Unable to open file for writing"
-	end
+  local file = io.open(filePath, "w")
+  if not file then
+    return false, "Unable to open file for writing"
+  end
 
-	file:write(content)
-	file:close()
+  file:write(content)
+  file:close()
 
-	return true
+  return true
 end
 
 Helpers.createFolderIfNotExists = function(folderPath)
-	if not lfs.attributes(folderPath, "mode") then
-		lfs.mkdir(folderPath)
-		return true
-	else
-		return false
-	end
+  if not lfs.attributes(folderPath, "mode") then
+    lfs.mkdir(folderPath)
+    return true
+  else
+    return false
+  end
 end
 
 return Helpers
